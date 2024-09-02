80e8041321ca60c3c0f8a626749f3dd45c3a727e
JairVazquez <jairvazquez604@gmail.com>
2024-09-01 18:47:59 -0600

    First Commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'media', // o 'class'
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
