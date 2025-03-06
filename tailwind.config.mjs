7e48f79df54c99df6e57004f1231e784386a8399
JairVazquez <jairvazquez604@gmail.com>
2025-03-06 02:40:55 -0600

    Add fonts, and background

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'media', // o 'class'
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: "media", // o 'class'
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ['"Bebas Neue"', "sans-serif"], // Para los títulos
+        subtitle: ['"Poppins"', "sans-serif"], // Para los subtítulos
+        body: ['"Roboto"', "sans-serif"], // Para los párrafos
+        description: ['"Lora"', "serif"], // Para las descripciones
+        nav: ['"Open Sans"', "sans-serif"], // Para la navegación
+		    page: ['"Bangers"', "sans-serif"], // Para el nombre de la página
+        price: ['"Raleway"', "sans-serif"], // Para los precios
+      },
+    },
+  },
+  plugins: [],
+};
